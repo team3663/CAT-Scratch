63a177cb51f4bf09f651cd0bf5c48931a5825c7d
Angelique Yang <angeliquenumber1@gmail.com>
2014-08-19 16:10:57 -0700

    willJr is alive and others

--- SalesmanDilema/src/salesmandilema/WillyLoman.java
+++ SalesmanDilema/src/salesmandilema/WillyLoman.java
@@ -1,60 +1,43 @@
 package salesmandilema;
 
 public class WillyLoman {
-    public int[] solveIt(int numberOfCities, int[][] cityDistances){
-        int[] sol = new int[numberOfCities];
+    public int[] solveIt(int[][] cityDistances){
+        int numberOfCities = cityDistances[0].length;
+        int[] sol = new int[numberOfCities+1];
         int temp;
         int farthest = 0;
         int farthestC = 0;
         
         for (int i = 0; i < numberOfCities; i++)
         {
+            for (int j = 1+i; j < numberOfCities; j++)
+            {
+                if (cityDistances[i][j] > farthest)
+                {
+                        farthest = cityDistances[i][j];
+                        farthestC = i;
+                }
+            }
+        }
+        for (int i = 0; i < numberOfCities; i++)
+        {
             sol[i] = i;
         }
-        for (int i = 0; i < numberOfCities; i++)
-        {
-            for (int j = 1; j < numberOfCities-1; j++)
-            {
-                if (cityDistances[sol[i]][sol[j]] > cityDistances[sol[i]][sol[j+1]])
-                {
-                    if (cityDistances[sol[i]][sol[j]] > farthest)
-                    {
-                        farthest = cityDistances[sol[i]][sol[j]];
-                        farthestC = sol[j];
-                    }
-                }
-                else
-                {
-                    if (cityDistances[sol[i]][sol[j+1]] > farthest)
-                    {
-                        farthest = cityDistances[sol[i]][sol[j+1]];
-                        farthestC = sol[j+1];
-                    }
-                }
-            }
-        }
-        for (int i = 0; i < numberOfCities; i++)
-        {
-            if (farthestC == i)
-            {
-                temp = sol[0];
-                sol[0] = sol[i];
-                sol[i] = temp;
-            }
-        }
+        sol[0] = farthestC;
+        sol[farthestC] = 0;
         
         for (int i = 0; i < numberOfCities-1; i++){
-            for (int j = 1; j < numberOfCities-i; j++)
+            for (int j = 1+i; j < numberOfCities-i; j++)
             {
-                if (cityDistances[sol[i]][sol[i+1]] > cityDistances[sol[i]][sol[i+j]])
+                if (cityDistances[sol[i]][sol[i+1]] > cityDistances[sol[i]][sol[j]])
                 {
                     temp = sol[i+1];
-                    sol[i+1] = sol[i+j];
-                    sol[i+j] = temp;
+                    sol[i+1] = sol[j];
+                    sol[j] = temp;
                 }
             }
         }
-        
+        sol[numberOfCities] = sol[0];
         return sol;
     }
 }
